90203884fb8cf0f950214113aa2abee3b0d8c9e1
Marie-Pierre SALVI <msalvi@KLEE.LAN.NET>
2016-08-22 09:04:40 +0200

    wip explore offices, chart with floors and tooltips with main departments

--- app/js/globalMapControl.js
+++ app/js/globalMapControl.js
@@ -106,5 +106,161 @@ var mapControl = {
 					callback();
 				});
 			});
-		}
+		},
+	smallMapPlot:
+		function(name, callback) {
+			var longTooltip = [];
+
+			d3.xml( server + "maps/" + name + ".svg",
+				function(error, documentFragment) {
+					if(error){
+						console.log(error);
+						return;
+					}
+
+					var svgNode,
+						map,
+						allTables,
+						allAvailables;
+
+					svgNode = documentFragment.getElementsByTagName("svg")[0];
+
+					map = document.getElementById(name);
+					map.appendChild(svgNode);
+
+					// mark all tables as available
+					allTables = d3.select("#tables").selectAll("g");
+					allTables.attr("class", "available");
+					/////////////////////////////
+					// for each people, search his table
+					/////////////////////////////
+					d3.json( server+"people", function(error, data) {
+						/////////////////////////////
+						// add names to each table
+						/////////////////////////////
+						var dataset = data,
+							table;
+
+						dataset.forEach(function(data, i) {
+							// data example: ["CN=Laurence EYRAUD-JOLY,OU=Klee SA,OU=Utilisateurs,DC=KLEE,DC=LAN,DC=NET",
+							//					{ "mail": ["Laurence.EYRAUDJOLY@kleegroup.com"], "physicalDeliveryOfficeName": ["La Boursidière : N4-D-01"], "cn": ["Laurence EYRAUD-JOLY"] }]
+							// only need data[1]
+							var d = data[1];
+							// split tableID into two parts.
+							// ex "La Boursidiere : N3-A-01" => ["La Boursidiere", "N3-A-01"]
+
+							if (d.physicalDeliveryOfficeName) {
+								var splitID = d.physicalDeliveryOfficeName[0].split(/\s+:\s+/);
+
+								// do following if we have the second part
+								if(splitID[1]){
+									table = d3.select("#"+splitID[1]);
+									// if found in map, change table color, add hover actions
+									if(table[0][0] !== null){
+										// mark as occupied
+										table.attr("class", "occupied");
+										table.select("rect")
+											.attr("id", d.cn[0])
+											.attr("fill", "#ff9900");
+									}
+								}
+							}
+						});
+
+						////////////////////////////////
+						// show all available tables
+						////////////////////////////////
+						allAvailables = d3.select("#tables").selectAll(".available");
+						allAvailables.selectAll("rect").attr("fill", "#99ff99");
+						callback();
+					});
+				});
+		},
+	buildTooltips: function(names){
+
+		names.forEach(function(element){
+
+			/*
+			 * get the departments in one map
+			 */
+			console.log(element);
+			d3.xml(server + "maps/" + element + ".svg",
+				function(error, documentFragment) {
+					if(error){
+						console.log("error at " + element + " " + error);
+						return;
+					}
+					d3.json(server + "people/	", function(error, data) {
+
+						var dataset = data;
+						var departmentNames = [];
+						var tooltip = "";
+
+						// for each mapName, we will produce the tooltip
+						// containing the main departments situated in this floor
+						dataset.forEach(function(data, i) {
+							// data example: ["CN=Laurence EYRAUD-JOLY,OU=Klee SA,OU=Utilisateurs,DC=KLEE,DC=LAN,DC=NET",
+							//					{ "mail": ["Laurence.EYRAUDJOLY@kleegroup.com"], "physicalDeliveryOfficeName": ["La Boursidière : N4-D-01"], "cn": ["Laurence EYRAUD-JOLY"] }]
+
+							// get main departments in this floor
+							var d0 = data[0].split(",");
+							var departmentName = d0[1].split("=");
+
+							var d = data[1];
+							if (d.physicalDeliveryOfficeName) {
+								var splitID = d.physicalDeliveryOfficeName[0].split(/\s+:\s+/);
+								if(splitID[1]){
+									var floor = splitID[1].split("-")[0];
+									if(floor == element){
+										if($.inArray(departmentName[1], departmentNames)<0){
+											// add department name in the array
+											departmentNames.push(departmentName[1]);
+										}
+									}
+								}
+							}
+						});
+						/*
+						 * build the associated tooltip string
+						 * if array not empty
+						 */
+						if(departmentNames !== undefined && departmentNames.length > 0){
+							departmentNames.forEach(function(name){
+								if(name !== null && name !== undefined && name !== ""){
+										tooltip += "<li>" + name + "</li>";
+								}
+							});
+						}
+						else {
+							tooltip = "No data for this floor.";
+						}
+
+						/*
+						 * create the tooltip object with mouseonver on the map
+						 */
+
+						// Define the div for the tooltip
+						var div = d3.select("#main").select(".tooltip")
+							.style("opacity", 0);
+						var map = d3.select("#"+element);
+						map.on("mouseover", function() {
+							//alert(longTooltip);
+							div.transition()
+								.duration(200)
+								.style("opacity", .9)
+								.style("left", (d3.event.pageX + 16) + "px")
+								.style("top", (d3.event.pageY + 16) + "px")
+								.style("height", 20*(departmentNames.length +1) + "px");
+
+							div.html("<ul>" + tooltip + "</ul>");
+						});
+						map.on("mouseout", function() {
+							div.transition()
+								.duration(500)
+								.style("opacity", 0);
+						});
+					});
+				});
+		});
+	}
 };
---
+++ app/js/justPlotMap.js
@@ -0,0 +1,39 @@
+/**
+ * Created by msalvi on 19/08/2016.
+ */
+/**
+ * plot map on the selector
+ */
+(function(window){
+
+  var mapNames = ["N0", "N1", "N2", "N3", "N4", "O4", "O3", "O2"];
+    var addEvtListenerOn = function(type, mapNames, where){
+        // tooltips for every floor
+        var toolTips = [];
+        where.addEventListener(type, function(){
+            var everyMap = [];
+            everyMap  = document.getElementsByClassName("small-map");
+            // if maps are already there, remove them
+            Array.from(everyMap).forEach( function(element){
+                if(element.hasChildNodes()){
+                    element.childNodes.forEach(function(node){
+                        node.remove();
+                    });
+                }
+            });
+            mapNames.forEach( function(element){
+                mapControl.mapName = element;
+                toolTips.push(mapControl.smallMapPlot(element, function() {}));
+            });
+        });
+        mapControl.buildTooltips(mapNames);
+    };
+
+    /**
+     * on title 'explorer les locaux'
+     */
+    addEvtListenerOn("load", mapNames, window);
+    addEvtListenerOn("click", mapNames, document.querySelector("#explore-title"));
+
+
+}(window));
