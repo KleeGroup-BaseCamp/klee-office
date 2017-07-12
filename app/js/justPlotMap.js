9ca03c74f9af7c0c22b67029b5d12cc7b47b5818
Alexandre MORIN <almorin@KLEE.LAN.NET>
2017-07-12 15:28:36 +0200

    Correction des conflits et màj de justPlotMap.js

--- app/js/justPlotMap.js
+++ app/js/justPlotMap.js
@@ -6,61 +6,48 @@
  */
 (function(window){
 
- /*NOT USED ANYMORE var mapNames = ["N0", "N1", "N2", "N3", "N4", "O4", "O3", "O2","O1"];
-    var addEvtListenerOn = function(type, mapNames, where){
+
+    var server="http://local-map/";
+    var myData=["","",""];
+    //var ok=0;	
+    myData[0]=document.getElementById("personal-firstName").textContent;
+    myData[1]=document.getElementById("personal-lastName").textContent;
+    /*$.when(d3.json(server + "currentOfficeName/" + myData[0] + "/" + myData[1], function(err, res){
+						myData[2] = res[0].name;
+						d3.select("#personal-desk").html(myData[2])}))
+	.then(function(myData){addEvtListenerOn("load", myData, window)});*/
+    
+    var addEvtListenerOn = function(type, myData){
 
         // tooltips for every floor
-        var toolTips = [];
-        where.addEventListener(type, function(){
-            var everyMap = [];
-            everyMap  = document.getElementsByClassName("small-map");
-            // if maps are already there, remove them
-            Array.from(everyMap).forEach( function(element){
-                if(element.hasChildNodes()){
-                    element.childNodes.forEach(function(node){
-                        node.remove();
-                    });
-                }
-            });
-            mapNames.forEach( function(element){
-                mapControl.mapName = element;
-                toolTips.push(mapControl.smallMapPlot(element, function() {}));
-            });
-
-            // erase normal map
-            if(mapControl.existMap){
-                d3.select(".map").select("svg").remove();
-            }
-            mapControl.existMap = false;
-            console.log("existMap = false");
-        });
-        mapControl.buildTooltips(mapNames);
-};*/
-    var server="http://localhost:3000/";
-    var myData=["Alain GOURLAY", ""];
-    d3.json(server+"currentOfficeName/"+myData[0].split(/ /)[0]+'/'+myData[0].split(/ /)[1],function(error,res){
-        myData[2] = res[0].name;
-    });
-
-    var addEvtListenerOn = function(type, myData, where){
-
-        // tooltips for every floor
-        var desk=myData[1];
+        var desk=myData[2];
+	console.log(myData);
+	console.log("C'est addEvtmachin!");
         var mapName=desk.split(/-/)[0];
-        where.addEventListener(type, function(){
-		    mapControl.eraseMap();
+	console.log(mapName);
+        //where.addEventListener(type, function(){
+		console.log("OUUUUUHouuuu");    
+		mapControl.eraseMap();
 		// if no map, show myMap
 		if (!mapControl.existMap) {
 			//mapControl.mapPlot(mapName, false, function() {});
 			mapControl.existMap = true;
+			console.log("cas 1 : IF");
 		}
 		// if other map, delete and show myMap
 		else if (mapControl.mapName !== mapName) {
 			d3.select(".map").select("svg").remove();
 			mapControl.mapName = mapName;
+			console.log("cas 2 : ELSE IF");
 			//mapControl.mapPlot(mapName, false, function() {});
-		}
-		$('<h1 class='+mapName+'>Etage '+mapName[1]+'<br/>('+mapName+')</h1>').prependTo($('#map-name'));
+		}else{
+			console.log("cas 3 : ELSE");}
+
+		if (mapName !== ""){
+			$('<h1 class='+mapName+'>Etage '+mapName[1]+'<br/>('+mapName+')</h1>').prependTo($('#map-name'));}
+		
+	
+
 
         d3.select("#navigation-chart")
 					.style("visibility", "hidden")
@@ -79,17 +66,30 @@
                     .style("padding","0px");
         d3.selectAll("#etages_withoutResult").style("visibility", "visible");
         d3.select("#title-default").html("MODE Navigation");
-        d3.select("#text-default").html("<img src=\"img/pin_home.png\" alt=\"My Position\" style=\"height:40px\" float:\"left\">Vous êtes étage "+mapName+" !");
+        if(mapName !== ""){
+	d3.select("#text-default").html("<img src=\"img/pin_home.png\" alt=\"My Position\" style=\"height:40px\" float:\"left\">Vous êtes étage "+mapName+" !");}
+	else{d3.select("#text-default").html("<img src=\"img/pin_home.png\" alt=\"My Position\" style=\"height:40px\" float:\"left\">Vous n'avez pas de bureau!");}
 
-        mapControl.mapPlot(myData,mapName,false,function(){})           
-        })
+        mapControl.mapPlot(myData,mapName,false,function(){});           
+       // })
     };
     /**
      * on title 'explorer les locaux'
      */
-    //addEvtListenerOn("load", mapNames, window);
-    addEvtListenerOn("load", myData, window);
-    //addEvtListenerOn("click", mapNames, document.querySelector("#home-label"));
+    
+     d3.json(server + "currentOfficeName/" + myData[0] + "/" + myData[1], function(err, res){
+						myData[2] = res[0].name;
+						console.log("test");
+						d3.select("#personal-desk").html(myData[2]);
+						addEvtListenerOn("load", myData)});
+	//.then(function(){
+	//console.log(myData);
+	//addEvtListenerOn("load", myData, window)});
+
+    
 
 
 }(window));
+
+
+
