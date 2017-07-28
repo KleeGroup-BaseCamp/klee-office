a30fccc06fcddda99a5803a7da5a7f76ccc98b39
Alexandre MORIN <almorin@KLEE.LAN.NET>
2017-07-28 17:15:46 +0200

    Add mapAdmin script + update admin/confi-list graphism

---
+++ app/js/mapAdmin.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var selectedElement = 0;
+var currentX = 0;
+var currentY = 0;
+var currentMatrix = 0;
+
+function selectElement(evt) {
+    console.log("selectElementEvt");
+    selectedElement = evt.target;
+    currentX = evt.clientX;
+    currentY = evt.clientY;
+    console.log("currentX = "+currentX+" || currentY = "+currentY);
+    console.log(selectedElement.getAttributeNS(null, "transform"));
+    currentMatrix = selectedElement.getAttributeNS(null, "transform").slice(7,-1).split(' ');
+
+    for(var i=0; i<currentMatrix.length; i++) {
+        currentMatrix[i] = parseFloat(currentMatrix[i]);
+     } 
+    selectedElement.setAttributeNS(null, "onmouseout", "deselectElement(evt)");
+    selectedElement.setAttributeNS(null, "onmouseup", "deselectElement(evt)");
+}
+
+function moveElement(evt){
+  dx = evt.clientX - currentX;
+  dy = evt.clientY - currentY;
+  currentMatrix[4] += dx;
+  currentMatrix[5] += dy;
+  newMatrix = "matrix(" + currentMatrix.join(' ') + ")";
+            
+  selectedElement.setAttributeNS(null, "transform", newMatrix);
+  currentX = evt.clientX;
+  currentY = evt.clientY;
+}
+
+function deselectElement(evt){
+  if(selectedElement != 0){
+    selectedElement.removeAttributeNS(null, "onmousemove");
+    selectedElement.removeAttributeNS(null, "onmouseout");
+    selectedElement.removeAttributeNS(null, "onmouseup");
+    selectedElement = 0;
+  }
+}
+
+$("#mode-admin").click(function(){
+    /*d3.select("#whole-map").select("svg").on("zoom",null)
+        .scale(1)
+        .translate([0,0]);*/
+    var desk = d3.select("#whole-map").select("svg").select("#tables").selectAll("g");
+    desk.classed("draggable", true)
+        .attr("cursor","move")
+        .style("cursor","")
+        .attr("transform","matrix(1 0 0 1 0 0)")
+        .attr("onmousedown","selectElement(evt)");
+    
+});
